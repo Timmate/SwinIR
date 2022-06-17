25b795a19dbb9b28de28ad79fa3f1aad0f7638b0
Timofei Kornev <timofei.kornev@protonmail.com>
2022-06-18 01:00:01 +0200

    Add skipping imgs larger than given res

--- main_test_swinir.py
+++ main_test_swinir.py
@@ -30,6 +30,7 @@ def main():
     parser.add_argument('--output_folder', type=str, default=None, help='folder to save output images to')
     parser.add_argument('--tile', type=int, default=None, help='Tile size, None for no tile during testing (testing as a whole)')
     parser.add_argument('--tile_overlap', type=int, default=32, help='Overlapping of different tiles')
+    parser.add_argument('--img_skip_res', type=int, default=180, help='Max width/height of images that are to be upscaled')
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -64,6 +65,8 @@ def main():
         imgname, img_lq, img_gt = get_image_pair(args, path)  # image to HWC-BGR, float32
         img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]], (2, 0, 1))  # HCW-BGR to CHW-RGB
         img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(device)  # CHW-RGB to NCHW-RGB
+        if img_lq.shape[-1] >= args.img_skip_res:  # for square img, last two dims are same
+            continue
 
         # inference
         with torch.no_grad():
